dcbc83a22c765b8825576f09f3860b7bb293bfba
Solomiia <nadija2379@gmail.com>
2021-12-25 15:01:20 +0100

    First Commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    //Перетворення дробового число в десяткове
+
+    float p,fracNume=25.0,fracDeno=32.0,d;
+    p=(fracNume/fracDeno);
+    printf("Fraction to Decimal = %f",p);
+    return(0);
+}
